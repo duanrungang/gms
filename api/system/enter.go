a936e58aaa722e97775594a4ee4427f819a73037
duanrungang <duanrungang@163.com>
2022-07-08 16:47:15 +0800

    feat: 新增菜单管理

--- api/system/enter.go
+++ api/system/enter.go
@@ -18,5 +18,6 @@ var (
 	userService      = service.ServiceGroupApp.SystemServiceGroup.UserService
 	initDBService    = service.ServiceGroupApp.SystemServiceGroup.InitDBService
 	casbinService    = service.ServiceGroupApp.SystemServiceGroup.CasbinService
+	baseMenuService  = service.ServiceGroupApp.SystemServiceGroup.BaseMenuService
 	authorityService = service.ServiceGroupApp.SystemServiceGroup.AuthorityService
 )
--- service/system/sys_menu.go
+++ service/system/sys_menu.go
@@ -1,9 +1,11 @@
 package system
 
 import (
+	"errors"
 	"gms/global"
 	"gms/model/common/request"
 	"gms/model/system"
+	"gorm.io/gorm"
 	"strconv"
 )
 
@@ -94,6 +96,77 @@ func (menuService *MenuService) getChildrenList(menu *system.SysMenu, treeMap ma
 		err = menuService.getChildrenList(&menu.Children[i], treeMap)
 	}
 	return err
+}
+
+//@author: [piexlmax](https://github.com/piexlmax)
+//@function: GetInfoList
+//@description: 获取路由分页
+//@return: list interface{}, total int64,err error
+
+func (menuService *MenuService) GetInfoList() (list interface{}, total int64, err error) {
+	var menuList []system.SysBaseMenu
+	treeMap, err := menuService.getBaseMenuTreeMap()
+	menuList = treeMap["0"]
+	for i := 0; i < len(menuList); i++ {
+		err = menuService.getBaseChildrenList(&menuList[i], treeMap)
+	}
+	return menuList, total, err
+}
+
+//@author: [piexlmax](https://github.com/piexlmax)
+//@function: getBaseChildrenList
+//@description: 获取菜单的子菜单
+//@param: menu *model.SysBaseMenu, treeMap map[string][]model.SysBaseMenu
+//@return: err error
+
+func (menuService *MenuService) getBaseChildrenList(menu *system.SysBaseMenu, treeMap map[string][]system.SysBaseMenu) (err error) {
+	menu.Children = treeMap[strconv.Itoa(int(menu.ID))]
+	for i := 0; i < len(menu.Children); i++ {
+		err = menuService.getBaseChildrenList(&menu.Children[i], treeMap)
+	}
+	return err
+}
+
+//@author: [piexlmax](https://github.com/piexlmax)
+//@function: AddBaseMenu
+//@description: 添加基础路由
+//@param: menu model.SysBaseMenu
+//@return: error
+
+func (menuService *MenuService) AddBaseMenu(menu system.SysBaseMenu) error {
+	if !errors.Is(global.GVA_DB.Where("name = ?", menu.Name).First(&system.SysBaseMenu{}).Error, gorm.ErrRecordNotFound) {
+		return errors.New("存在重复name，请修改name")
+	}
+	return global.GVA_DB.Create(&menu).Error
+}
+
+//@author: [piexlmax](https://github.com/piexlmax)
+//@function: getBaseMenuTreeMap
+//@description: 获取路由总树map
+//@return: treeMap map[string][]system.SysBaseMenu, err error
+
+func (menuService *MenuService) getBaseMenuTreeMap() (treeMap map[string][]system.SysBaseMenu, err error) {
+	var allMenus []system.SysBaseMenu
+	treeMap = make(map[string][]system.SysBaseMenu)
+	err = global.GVA_DB.Order("sort").Preload("MenuBtn").Preload("Parameters").Find(&allMenus).Error
+	for _, v := range allMenus {
+		treeMap[v.ParentId] = append(treeMap[v.ParentId], v)
+	}
+	return treeMap, err
+}
+
+//@author: [piexlmax](https://github.com/piexlmax)
+//@function: GetBaseMenuTree
+//@description: 获取基础路由树
+//@return: menus []system.SysBaseMenu, err error
+
+func (menuService *MenuService) GetBaseMenuTree() (menus []system.SysBaseMenu, err error) {
+	treeMap, err := menuService.getBaseMenuTreeMap()
+	menus = treeMap["0"]
+	for i := 0; i < len(menus); i++ {
+		err = menuService.getBaseChildrenList(&menus[i], treeMap)
+	}
+	return menus, err
 }
 
 //AddMenuAuthority
